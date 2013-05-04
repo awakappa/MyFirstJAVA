33420a5bc1ecb3374273c39dfed9bee05add0d6a
Shingo Kuroiwa <awakappa@google.com>
2013-05-04 19:28:10 +0900

    練習用JAVAプログラム

---
+++ MyFistJava/src/hoge/Hello.java
@@ -0,0 +1,14 @@
+package hoge;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloJAVA!!");
+		System.out.println("HelloGit!!");
+	}
+
+}
